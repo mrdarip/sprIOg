a4bf8e4840d6d210989e296d2d0767ea75eb7515
mrdarip <darioelas2005@gmail.com>
2024-08-08 15:07:07 +0200

    fixed Input class

--- Input.ino
+++ Input.ino
@@ -10,25 +10,38 @@ public:
         // Clean up resources here
     }
 
-    // Member functions
     void updateState() {
-        
+        for(int i = 0; i < addedButtons; i++) {
+            buttons[i].updateState();
+        }
+    }
+
+    Button getButton(char id) {
+        return buttons[indexOf(id,chars,8)];
+    }
+
+    Button b(char id) {
+        return getButton(id);
     }
 
     void addButton(char id,Button button) {
-        Button* temp = new Button[buttonCount + 1];
-        for (int i = 0; i < buttonCount; i++)
-        {
-            temp[i] = buttons[i];
-        }
-        temp[buttonCount] = button;
-        delete[] buttons;
-        buttons = temp;
-        buttonCount++;
+        this->buttons[indexOf(id,chars,8)] = button;
+        this->addedButtons++;
     }
 
 private:
     // Private members
-    Button* buttons;
-    int buttonCount;
+    Button[] buttons = new Buttons[8];
+    int addedButtons;
+
+    const chars = ['w','a','s','d','i','j','k','l'];
 };
+
+int indexOf(char c, const char* chars, int size) {
+    for (int i = 0; i < size; i++) {
+        if (chars[i] == c) {
+            return i;
+        }
+    }
+    return -1;
+}
