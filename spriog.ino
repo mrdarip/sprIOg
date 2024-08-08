2f223c23338d9fa021444029d769237b789c9879
mrdarip <darioelas2005@gmail.com>
2024-08-08 10:35:51 +0200

    implemented print up tu n dirs or files function

--- spriog.ino
+++ spriog.ino
@@ -59,6 +59,34 @@ void printDirectory(File dir, int numTabs) {
   }
 }
 
+int printNdirs(File dir, int numTabs, int nDirs) {//returns the number of directories printed
+  int hasToPrint = nDirs;
+  while (nDirs > 0) {
+    File entry = dir.openNextFile();
+
+    if (!entry) {
+      // no more files
+      break;
+    }
+
+    for (uint8_t i = 0; i < numTabs; i++) {
+      print("\t");
+    }
+
+    print(entry.name());
+    nDirs--;
+
+    if (entry.isDirectory()) {
+      println("/");
+      nDirs -= printNdirs(entry, numTabs + 1, nDirs);
+    }
+
+    entry.close();
+  }
+
+  return hasToPrint - nDirs;
+}
+
 Scene sampleScene = Scene(0, []() {
   tft.fillScreen(ST77XX_BLACK);
   tft.setCursor(0, 0);
